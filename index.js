9bb79814aaf6766503d04fc0fb1d01d86da988df
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-08-20 11:24:34 +0600

    fixes

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import app from "./app.js";
 import config from "./config/index.js";
 import colors from "colors";
 
-const connectDB = async () => {
-	try {
-		console.log(config.MONGODB_URI);
-		const conn = await mongoose.connect(config.MONGODB_URI);
-		console.log(
-			`MongoDB Connected: ${conn.connection.host}🔥🔥🔥🚀🚀`.green.underline
-		);
-		app.listen(config.port, () => {
-			console.log(`Server listening on ${config.port}`.yellow.underline);
-		});
-	} catch (err) {
-		console.error(`Error:${err.message}`.red.underline.bold);
-		process.exit(1);
+let isConnected = false;
+
+async function connectDB() {
+	if (!isConnected) {
+		try {
+			console.log(config.MONGODB_URI);
+			const conn = await mongoose.connect(config.MONGODB_URI);
+			console.log(
+				`MongoDB Connected: ${conn.connection.host}🔥🔥🔥🚀🚀`.green.underline
+			);
+			isConnected = true;
+		} catch (err) {
+			console.error(`Error:${err.message}`.red.underline.bold);
+			throw err;
+		}
 	}
-};
-connectDB();
+}
+
+export default async function handler(req, res) {
+	await connectDB();
+	app(req, res);
+}
