36b73c9ec490138fd721c53f83c52a396cf2649b
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-08-20 19:17:43 +0600

    some  changes

--- controllers/user.js
+++ controllers/user.js
@@ -26,46 +26,50 @@ const authUser = async (req, res) => {
   }
 };
 
-const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
-  console.log("registerUser req.body: ", req.body);
-  if (!username || !password) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid User Data",
-    });
-  }
+const registerUser = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    console.log("registerUser req.body: ", req.body);
+    if (!username || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid User Data",
+      });
+    }
 
-  const userExists = await User.findOne({ email });
-  const usernameExists = await User.findOne({ username });
-  console.log("userExists: ", userExists);
+    const userExists = await User.findOne({ email });
+    const usernameExists = await User.findOne({ username });
+    console.log("userExists: ", userExists);
 
-  if (userExists || usernameExists) {
-    return res.status(400).json({
-      success: false,
-      message: "User already exists",
-    });
-  }
+    if (userExists || usernameExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
 
-  const user = new User({
-    username,
-    email,
-  });
-  user.setPassword(password);
-  await user.save();
+    const user = new User({
+      username,
+      email,
+    });
+    user.setPassword(password);
+    await user.save();
 
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid user data",
-    });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid user data",
+      });
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
